07eefc40d2f239e8760bf06d76b491f078fa83c8
mg859337 <48734633+mg859337@users.noreply.github.com>
2019-05-23 11:26:40 -0400

    Add files via upload

---
+++ StringTie_Mouse.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Parameters for computing nodes
+
+# Request Resources
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=55G
+
+# Specify Job name
+#SBATCH -J STRINGTIE_TPM
+
+# Specify Job Output and Input Files
+#SBATCH -o outputfilestringtie.out
+#SBATCH -e errorfilestringtie.err
+
+# stringtie <aligned_reads.bam> [options]*
+
+module load stringtie
+
+# folder name changes based on dataset
+folder=foldernamegoeshere
+echo "folder = $folder"
+
+for aligned in *.bam; do
+OUTPUT=$(basename ${aligned} |cut -f 1 -d ".");
+echo "Output = $OUTPUT";
+
+stringtie ${aligned} -o ~/data/mgura/${folder}/gtfs/${OUTPUT}.gtf -G /users/mgura/data/mgura/genomes/Mouse/UCSC_mm10_Mus_musculus/UCSC_mm10_genes.gtf -l ${OUTPUT} -A ~/data/mgura/${folder}/tabs/${OUTPUT}.tab -e
+done
