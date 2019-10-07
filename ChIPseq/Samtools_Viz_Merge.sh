fda1c7ab15a5ee30f51bcf67eced78431876a1d5
mg859337 <48734633+mg859337@users.noreply.github.com>
2019-10-07 16:47:36 -0400

    Add files via upload

---
+++ ChIPseq/Samtools_Viz_Merge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# The following specifies computing cluster parameters
+
+# Request Resources
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=108G
+
+# Specify Job Name
+#SBATCH -J Merge_Kojima
+
+#Specify Output and Input Files
+#SBATCH -o outputbowtie2.out
+#SBATCH -e errorbowtie2.err
+
+module load samtools
+
+#Merging FLAG bam files
+
+samtools merge FF_FLAG.bam FF_FLAG_1_filtered.bam FF_FLAG_2_filtered.bam FF_FLAG_3_filtered.bam
+
+#Merging FLAG bam files
+samtools merge FF_Input.bam FF_Input_1_filtered.bam FF_Input_2_filtered.bam FF_Input_3_filtered
