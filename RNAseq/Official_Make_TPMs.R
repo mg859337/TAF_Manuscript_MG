a8c370f134961f11d06ad6767e5b5faf424ed6df
mg859337 <48734633+mg859337@users.noreply.github.com>
2021-03-17 14:30:33 -0400

    Update Official_Make_TPMs.R

--- RNAseq/Official_Make_TPMs.R
+++ RNAseq/Official_Make_TPMs.R
@@ -7,7 +7,7 @@ FileNames <- list.files(path="")
 name <- gsub(".gtf","",FileNames)
 
 
-# The gtf organizing loop for mice
+# The gtf organizing loop
 j <- 1
 
 # the number after "<" changes depending on how many count tables are being merged
@@ -81,76 +81,3 @@ solo <- dataMerge[,names(dataMerge) %in% colnames(dataMerge)[grepl("TPM",colname
 final <- cbind(dataMerge[,1], solo)
 
 write.csv(final, "All_TPMs.csv", row.names = F)
-
-
-
-
-# The gtf organizing loop for humans
-j <- 1
-while(j<5){
-  temp <- read.delim(list.files()[j], header = F, row.names = NULL, comment.char="#", stringsAsFactors=FALSE)
-  temp2 <- subset(temp, V3 != "exon")
-  temp3 <- read.table(text = as.character(temp2[,9]), sep = ";")
-  
-  geneid <- as.data.frame(gsub(".* ","",temp3[,1]))
-  transid <- as.data.frame(gsub(".* ","",temp3[,2]))
-  cov <- as.data.frame(gsub(".* ","",temp3[,3]))
-  fpkm <- as.data.frame(gsub(".* ","",temp3[,4]))
-  tpm <- as.data.frame(gsub(".* ","",temp3[,5]))
-  
-  temp4 <- cbind(geneid, transid, cov, fpkm, tpm)
-  
-  colnames(temp4) <- c("Gene_ID", "Transcript_ID", "Coverage", "FPKM","TPM")
-  
-  write.csv(temp4, file = paste("/Users/Megan Gura/Documents/Freiman_Lab/Computational/RNA_seq/TPM_Calc/", name[j], "_organized.csv", sep = ""), row.names = F)
-  
-  j <- j + 1
-  
-}
-
-# Merging the transcripts into just gene names
-
-Files <- list.files(path="/Users/Megan Gura/Documents/Freiman_Lab/Computational/RNA_seq/TPM_Calc/")
-First <- read.csv(file=paste("/Users/Megan Gura/Documents/Freiman_Lab/Computational/RNA_seq/TPM_Calc/", Files[1], sep=""),
-                  header=T)
-
-
-FirstFix <- aggregate(cbind(Coverage, FPKM, TPM) ~ Gene_ID, data = First, FUN = sum)
-
-Second <- read.csv(file=paste("/Users/Megan Gura/Documents/Freiman_Lab/Computational/RNA_seq/TPM_Calc/", Files[2], sep=""),
-                   header=T)
-
-SecondFix <- aggregate(cbind(Coverage, FPKM, TPM) ~ Gene_ID, data = Second, FUN = sum)
-
-dataMerge <- merge(FirstFix, SecondFix, by="Gene_ID", all=T)
-
-x <- colnames(dataMerge)
-fixednames <- gsub(".x", paste("_", name[1]),x)
-fixednames <- gsub(".y", paste("_", name[2]),fixednames)
-fixednames <- gsub(" ", "", fixednames)
-
-colnames(dataMerge) <- fixednames
-
-for(i in 3:length(Files)){ 
-  ReadInMerge <- read.csv(file=paste("/Users/Megan Gura/Documents/Freiman_Lab/Computational/RNA_seq/TPM_Calc/", Files[i], sep=""),
-                          header=T)
-  FixInMerge <- aggregate(cbind(Coverage, FPKM, TPM) ~ Gene_ID, data = ReadInMerge, FUN = sum)
-  dataMerge <- merge(dataMerge, FixInMerge, by="Gene_ID", all = T)
-  
-  fixednames <- colnames(dataMerge)
-  fixednames <- gsub(".x", paste("_", name[i-1]),fixednames)
-  fixednames <- gsub(".y", paste("_", name[i]),fixednames)
-  fixednames <- gsub(" ", "", fixednames)
-  colnames(dataMerge) <- fixednames
-  
-}
-
-setwd("/RNA_seq/")
-
-write.csv(dataMerge, "All_TPM_Maths.csv", row.names = F)
-
-solo <- dataMerge[,names(dataMerge) %in% colnames(dataMerge)[grepl("TPM",colnames(dataMerge))]]
-final <- cbind(dataMerge[,1], solo)
-
-write.csv(final, "All_TPMs.csv", row.names = F)
-
